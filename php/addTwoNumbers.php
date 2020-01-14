3eebd9df6ab85ee0a417324893acf4a413436a38
罗晓俊 <luoxiaojun1992@sina.cn>
2020-01-14 15:32:23 +0800

    Create addTwoNumbers.php

---
+++ php/addTwoNumbers.php
@@ -0,0 +1,45 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $l1
+     * @param ListNode $l2
+     * @return ListNode
+     */
+    function addTwoNumbers($l1, $l2) {
+        $sum = null;
+        $currentBit = null;
+        $carryFlag = false;
+
+        while ((!is_null($l1)) || (!is_null($l2)) || $carryFlag) {
+            $l1Val = $l1 ? $l1->val : 0;
+            $l2Val = $l2 ? $l2->val : 0;
+            $valSum = $l1Val + $l2Val;
+            if ($carryFlag) {
+                $valSum += 1;
+            }
+            $carryFlag = $valSum >= 10;
+            $realValSum = ($valSum < 10) ? $valSum : ($valSum - 10);
+            if (is_null($sum)) {
+                $sum = new ListNode($realValSum);
+                $currentBit = $sum;
+            } else {
+                $newSum = new ListNode($realValSum);
+                $currentBit->next = $newSum;
+                $currentBit = $newSum;
+            }
+
+            $l1 = $l1->next;
+            $l2 = $l2->next;
+        }
+
+        return $sum ? : new ListNode(0);
+    }
+}
