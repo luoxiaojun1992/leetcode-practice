fda5a3ec8f28d5a602727cd911e053084cb476dc
罗晓俊 <luoxiaojun1992@sina.cn>
2020-01-14 15:31:53 +0800

    Create twoSum.php

---
+++ php/twoSum.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $diffMap = [];
+
+        foreach($nums as $i => $num) {
+            if (isset($diffMap[$num])) {
+                return [$diffMap[$num], $i];
+            }
+            
+            $diffMap[$target - $num] = $i;
+        }
+    }
+}
