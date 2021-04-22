932f3b9f973e80bb4efdb02e06b7fb593c58ae01
luoxiaojun <luoxiaojun1992@sina.cn>
2021-04-22 13:11:59 +0800

    add maxArea.php

---
+++ php/maxArea.php
@@ -0,0 +1,36 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height)
+    {
+        $maxArea = 0;
+        $lineCount = count($height);
+
+        for ($i = 0, $j = $lineCount - 1; ($i < ($lineCount - 1)) && ($j > $i);) {
+            $newArea = ($j - $i) * min($height[$i], $height[$j]);
+            if ($newArea > $maxArea) {
+                $maxArea = $newArea;
+            }
+
+            if ($height[$i] < $height[$j]) {
+                ++$i;
+            } else {
+                --$j;
+            }
+        }
+
+        return $maxArea;
+    }
+}
+
+//[1,2,3,4,5,25,24,3,4]
+//[1,3,2,5,25,24,5]
+
+var_dump(
+    (new Solution())->maxArea([1,2,3,4,5,25,24,3,4])
+);
